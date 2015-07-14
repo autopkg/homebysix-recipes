18e299cde4c0e68e8810dedc7aeff3b11caae950
Elliot Jordan <elliot@elliotjordan.com>
2015-07-14 08:49:23 -0700

    Updated to latest version of GitHubReleasesInfoProvider.

--- ADPassMon/GitHubReleasesInfoProvider.py
+++ ADPassMon/GitHubReleasesInfoProvider.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 #
-# Copyright 2014 Timothy Sutton
+# Copyright 2014-2015 Timothy Sutton
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,91 +13,180 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""See docstring for GitHubReleasesInfoProvider class"""
 
-import json
+# Disabling warnings for env members and imports that only affect recipe-
+# specific processors.
+#pylint: disable=e1101,f0401
+
+import re
 import urllib2
 
+import autopkglib.github
 from autopkglib import Processor, ProcessorError
 
 __all__ = ["GitHubReleasesInfoProvider"]
 
-# http://developer.github.com/v3/repos/releases
-
 class GitHubReleasesInfoProvider(Processor):
-    description = ("Get URL and version info from the latest release on a GitHub project's releases page.")
+    #pylint: disable=missing-docstring
+    description = ("Get metadata from the latest release from a GitHub project "
+                   "using the GitHub Releases API.")
     input_variables = {
+        "asset_regex": {
+            "required": False,
+            "description": ("If set, return only a release asset that "
+                            "matches this regex.")
+        },
         "github_repo": {
             "required": True,
-            "description": ("Name of a GitHub user and repo, ie. 'MagerValp/AutoDMG'")
+            "description": ("Name of a GitHub user and repo, ie. "
+                            "'MagerValp/AutoDMG'")
         },
-        "github_auth_token": {
+        "include_prereleases": {
             "required": False,
-            "description": "API token to send with request. Without this, your client is rate-limited to 60 requests per hour."
-        }
+            "description": ("If set to True or a non-empty value, include "
+                            "prereleases.")
+        },
+        "sort_by_highest_tag_names": {
+            "required": False,
+            "description": ("Set this to have releases sorted by highest "
+                            "to lowest tag version. By default, releases "
+                            "are sorted descending by date posted. This "
+                            "changes this behavior for cases where an 'older' "
+                            "release may be posted later.")
+        },
     }
     output_variables = {
+        "release_notes": {
+            "description": ("Full release notes body text from the chosen "
+                            "release.")
+        },
         "url": {
-            "description": "URL for the first asset found for the project's latest release."
+            "description": ("URL for the first asset found for the project's "
+                            "latest release.")
         },
         "version": {
-            "description": "Version info parsed, naively derived from the release's tag."
-        }
+            "description": ("Version info parsed, naively derived from the "
+                            "release's tag.")
+        },
     }
 
     __doc__ = description
 
-    API_BASE_URL = "https://api.github.com"
-    def call_api(self, endpoint, query=None, accept="application/vnd.github.v3+json", token=None):
-        """Return dict from JSON result of an API call. Endpoints begin with a forward-slash."""
-        url = self.API_BASE_URL + endpoint
-        if query:
-            url += "?" + query
-        req = urllib2.Request(url)
-        req.add_header("Accept", accept)
-        if token:
-            req.add_header("Authorization", "token %s" % token)
-        json_resp = urllib2.urlopen(req)
-        data = json.load(json_resp)
-        return data or None
+
+    def get_releases(self, repo):
+        """Return a list of releases dicts for a given GitHub repo. repo must
+        be of the form 'user/repo'"""
+        #pylint: disable=no-self-use
+        releases = None
+        github = autopkglib.github.GitHubSession()
+        releases_uri = "/repos/%s/releases" % repo
+        try:
+            (releases, status) = github.call_api(releases_uri)
+        # Catch a 404
+        except urllib2.HTTPError as err:
+            raise ProcessorError("GitHub API returned an error: '%s'." % err)
+        if status != 200:
+            raise ProcessorError(
+                "Unexpected GitHub API status code %s." % status)
+
+        if not releases:
+            raise ProcessorError("No releases found for repo '%s'" % repo)
+
+        return releases
 
 
-    def get_asset_url(self, repo, asset_id, token=None):
-        url = self.API_BASE_URL + "/repos/%s/releases/assets/%s" % (
-                repo, asset_id)
-        req = urllib2.Request(url)
-        req.add_header("Accept", "application/octet-stream")
-        if token:
-            req.add_header("Authorization", "token %s" % token)
-        urlfd = urllib2.urlopen(req)
-        asset_url = urlfd.geturl()
-        return asset_url
+    def select_asset(self, releases, regex):
+        """Iterates through the releases in order and determines the first
+        eligible asset that matches the criteria. Sets the selected release
+        and asset data in class variables.
+        - Release 'type' depending on whether 'include_prereleases' is set
+        - If 'asset_regex' is set, whether the asset's 'name' (the filename)
+          matches the regex. If not, then the first asset will be
+          returned."""
+        selected = None
+        for rel in releases:
+            if selected:
+                break
+            if rel["prerelease"] and not self.env.get("include_prereleases"):
+                continue
+
+            assets = rel.get("assets")
+            if not assets:
+                continue
+
+            for asset in assets:
+                if not regex:
+                    selected = (rel, asset)
+                    break
+                else:
+                    if re.match(regex, asset["name"]):
+                        self.output("Matched regex '%s' among asset(s): %s" % (
+                            regex,
+                            ", ".join([x["name"] for x in assets])))
+                        selected = (rel, asset)
+                        break
+        if not selected:
+            raise ProcessorError(
+                "No release assets were found that satisfy the criteria.")
+
+        #pylint: disable=w0201
+        # We set these in the class to avoid passing more objects around
+        self.selected_release = selected[0]
+        self.selected_asset = selected[1]
+        self.output("Selected asset '%s' from release '%s'" %
+                    (self.selected_asset["name"],
+                     self.selected_release["name"]))
+
+
+    def process_release_asset(self):
+        """Extract what we need from the release and chosen asset, set env
+        variables"""
+        tag = self.selected_release["tag_name"]
+        # Versioned tags usually start with 'v'
+        if tag.startswith("v"):
+            tag = tag[1:]
+
+        self.env["url"] = self.selected_asset["browser_download_url"]
+        self.env["version"] = tag
 
 
     def main(self):
-        repo = self.env.get("github_repo")
-        releases_uri = "/repos/%s/releases" % repo
-        releases = self.call_api(releases_uri,
-                            token=self.env.get("github_auth_token"))
-        if releases:
-            # we very naively assume (for now) that the latest release is first in the list
-            latest = releases[0]
-        else:
-            raise ProcessorError("No releases found for repo '%s'" % repo)
+        # Get our list of releases
+        releases = self.get_releases(self.env["github_repo"])
+        if self.env.get("sort_by_highest_tag_names"):
+            from operator import itemgetter
 
-        tag = latest["tag_name"]
-        assets = latest.get("assets")
-        if assets:
-            asset = assets[0]
-        asset_id = asset["id"]
+            def loose_compare(this, that):
+                from distutils.version import LooseVersion
+                return cmp(LooseVersion(this), LooseVersion(that))
 
-        asset_url = self.get_asset_url(repo, asset_id)
-        self.env["url"] = asset_url
+            releases = sorted(releases,
+                              key=itemgetter("tag_name"),
+                              cmp=loose_compare,
+                              reverse=True)
 
+        # Store the first eligible asset
+        self.select_asset(releases, self.env.get("asset_regex"))
+
+        # Record the url
+        self.env["url"] = self.selected_asset["browser_download_url"]
+
+        # Get a version string from the tag name
+        tag = self.selected_release["tag_name"]
+        # Versioned tags usually start with 'v'
         if tag.startswith("v"):
-            self.env["version"] = tag[1:]
-        else:
-            self.env["version"] = tag
+            tag = tag[1:]
+        self.env["version"] = tag
+
+        # Record release notes
+        self.env["release_notes"] = self.selected_release["body"]
+        # The API may return a JSON null if no body text was provided,
+        # but we cannot ever store a None/NULL in an env.
+        if not self.env["release_notes"]:
+            self.env["release_notes"] = ""
+
 
 if __name__ == "__main__":
-    processor = GitHubReleasesInfoProvider()
-    processor.execute_shell()
+    PROCESSOR = GitHubReleasesInfoProvider()
+    PROCESSOR.execute_shell()
