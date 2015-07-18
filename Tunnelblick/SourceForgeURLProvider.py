9f04a64f7c94502bd95828ddaacd328dfe55ffb7
Elliot Jordan <elliot@elliotjordan.com>
2015-07-18 05:49:57 -0700

    Whitespace cleanup. (Tabs to spaces.)

--- Tunnelblick/SourceForgeURLProvider.py
+++ Tunnelblick/SourceForgeURLProvider.py
@@ -12,65 +12,65 @@ __all__ = ["SourceForgeURLProvider"]
 FILE_INDEX_URL = 'http://sourceforge.net/api/file/index/project-id/%s/rss'
 
 class SourceForgeURLProvider(Processor):
-	'''Provides URL to the latest file that matches a pattern for a particular SourceForge project.'''
+    '''Provides URL to the latest file that matches a pattern for a particular SourceForge project.'''
 
-	input_variables = {
-		'SOURCEFORGE_PROJECT_ID': {
-			'required': True,
-			'description': 'Numerical ID of SourceForge project',
-			},
-		'SOURCEFORGE_FILE_PATTERN': {
-			'required': True,
-			'description': 'Pattern to match SourceFile files on',
-			},
-	}
-	output_variables = {
-		'url': {
-			'description': 'URL to the latest SourceForge project download'
-		}
-	}
+    input_variables = {
+        'SOURCEFORGE_PROJECT_ID': {
+            'required': True,
+            'description': 'Numerical ID of SourceForge project',
+            },
+        'SOURCEFORGE_FILE_PATTERN': {
+            'required': True,
+            'description': 'Pattern to match SourceFile files on',
+            },
+    }
+    output_variables = {
+        'url': {
+            'description': 'URL to the latest SourceForge project download'
+        }
+    }
 
-	description = __doc__
+    description = __doc__
 
-	def get_sf_file_url(self, proj_id, pattern):
-		flisturl = FILE_INDEX_URL % proj_id
+    def get_sf_file_url(self, proj_id, pattern):
+        flisturl = FILE_INDEX_URL % proj_id
 
-		try:
-			f = urllib2.urlopen(flisturl)
-			rss = f.read()
-			f.close()
-		except BaseException as e:
-			raise ProcessorError('Could not retrieve RSS feed %s' % flisturl)
+        try:
+            f = urllib2.urlopen(flisturl)
+            rss = f.read()
+            f.close()
+        except BaseException as e:
+            raise ProcessorError('Could not retrieve RSS feed %s' % flisturl)
 
-		re_file = re.compile(self.env.get('SOURCEFORGE_FILE_PATTERN'), re.I)
+        re_file = re.compile(self.env.get('SOURCEFORGE_FILE_PATTERN'), re.I)
 
-		rss_parse = parseString(rss)
+        rss_parse = parseString(rss)
 
-		items = []
+        items = []
 
-		for i in  rss_parse.getElementsByTagName('item'):
-			pubDate = i.getElementsByTagName('pubDate')[0].firstChild.nodeValue
-			link = i.getElementsByTagName('link')[0].firstChild.nodeValue
+        for i in  rss_parse.getElementsByTagName('item'):
+            pubDate = i.getElementsByTagName('pubDate')[0].firstChild.nodeValue
+            link = i.getElementsByTagName('link')[0].firstChild.nodeValue
 
-			pubDatetime = datetime.datetime.strptime(pubDate, '%a, %d %b %Y %H:%M:%S UT')
+            pubDatetime = datetime.datetime.strptime(pubDate, '%a, %d %b %Y %H:%M:%S UT')
 
-			if re_file.search(link):
-				items.append((pubDatetime, link),)
+            if re_file.search(link):
+                items.append((pubDatetime, link),)
 
-		items.sort(key=lambda r: r[0])
+        items.sort(key=lambda r: r[0])
 
-		if len(items) < 1:
-			raise ProcessorError('No matched files')
+        if len(items) < 1:
+            raise ProcessorError('No matched files')
 
-		return items[-1][1]
+        return items[-1][1]
 
-	def main(self):
-		proj_id  = self.env.get('SOURCEFORGE_PROJECT_ID')
-		file_pat = self.env.get('SOURCEFORGE_FILE_PATTERN')
+    def main(self):
+        proj_id  = self.env.get('SOURCEFORGE_PROJECT_ID')
+        file_pat = self.env.get('SOURCEFORGE_FILE_PATTERN')
 
-		self.env['url'] = self.get_sf_file_url(proj_id, file_pat)
-		self.output('File URL %s' % self.env['url'])
+        self.env['url'] = self.get_sf_file_url(proj_id, file_pat)
+        self.output('File URL %s' % self.env['url'])
 
 if __name__ == '__main__':
-	processor = SourceForgeURLProvider()
-	processor.execute_shell()
+    processor = SourceForgeURLProvider()
+    processor.execute_shell()
