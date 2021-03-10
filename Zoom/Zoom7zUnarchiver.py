e78f46dda48a5fef8a9ee22f2d1510c397c90e58
James Stewart <james.stewart@hcl.com>
2021-03-10 12:43:24 -0500

    fix pylint action to check correct locations, force new check

--- Zoom/Zoom7zUnarchiver.py
+++ Zoom/Zoom7zUnarchiver.py
@@ -158,7 +158,7 @@ class Zoom7zUnarchiver(Processor):
             elif fmt.endswith("bzip2"):
                 cmd.append("-j")
 
-        # Call shell command.
+        # Call the shell command.
         try:
             proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             (_, stderr) = proc.communicate()
