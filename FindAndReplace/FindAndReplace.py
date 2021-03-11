aff3305232b663497a10ddc12c88bea0368f62df
Elliot Jordan <elliot@elliotjordan.com>
2021-03-10 20:51:31 -0800

    Remove unused imports

--- FindAndReplace/FindAndReplace.py
+++ FindAndReplace/FindAndReplace.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from autopkglib import Processor, ProcessorError  # noqa: F401
+from autopkglib import Processor  # noqa: F401
 
 __all__ = ["FindAndReplace"]
 
